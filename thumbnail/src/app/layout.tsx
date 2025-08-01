e2630f1cd7eead22a457a6defd1ac3ad0f643fa0
Harshal Lade <harshal.lade@spit.ac.in>
2025-08-01 11:00:29 +0530

    middleware issue resolved

--- thumbnail/src/app/layout.tsx
+++ thumbnail/src/app/layout.tsx
@@ -1,18 +1,16 @@
-// In your main layout file (e.g., layout.tsx)
-'use client';
-
-
 import { ClerkProvider } from '@clerk/nextjs';
 import '../styles/globals.css';
 
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
- 
-
   return (
     <html lang="en">
       <body>
-        <ClerkProvider>
+        <ClerkProvider
+          afterSignInUrl="/dashboard"
+          afterSignUpUrl="/dashboard"
+          signInUrl="/sign-in"
+          signUpUrl="/sign-up"
+        >
           {children}
         </ClerkProvider>
       </body>
